f7172d4f1df921d70eb1f4405ebb909e142db008
veruppp <pp4412542@gmail.com>
2025-07-04 14:30:21 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+// app/layout.tsx
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "v-cloud",
+  description: "Simple cloud hosting",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
